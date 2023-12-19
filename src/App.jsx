94cb16ecff5483c7b523124497499e25b3dc72d1
Gádor Heras <gador_1424@hotmail.com>
2023-12-19 21:45:52 +0100

    feature: includes toast provider for errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
+
+import { ToastProvider } from './shared/ui/context/toast-provider';
 import { CartProvider } from './smartphones/ui/context/cart-provider';
 import { ProductListPage } from './smartphones/ui/pages/plp/product-list-page';
 import { ProductDetailsPage } from './smartphones/ui/pages/pdp/product-details-page';
@@ -8,19 +10,24 @@ export const App = () => {
     return (
         <CartProvider>
             <BrowserRouter>
-                <SmartphonesProvider>
-                    <Routes>
-                        <Route
-                            path="/"
-                            element={<Navigate to="/smartphones" replace />}
-                        />
-                        <Route path="/smartphones" element={<ProductListPage />} />
-                        <Route
-                            path="/smartphones/:productId"
-                            element={<ProductDetailsPage />}
-                        />
-                    </Routes>
-                </SmartphonesProvider>
+                <ToastProvider>
+                    <SmartphonesProvider>
+                        <Routes>
+                            <Route
+                                path="/smartphones"
+                                element={<ProductListPage />}
+                            />
+                            <Route
+                                path="/smartphones/:productId"
+                                element={<ProductDetailsPage />}
+                            />
+                            <Route
+                                path="*"
+                                element={<Navigate to="/smartphones" replace />}
+                            />
+                        </Routes>
+                    </SmartphonesProvider>
+                </ToastProvider>
             </BrowserRouter>
         </CartProvider>
     );
